3c76942b946f7e64c5b5385becc4991999a4d493
jerrymokey <103255405+jerrymokey@users.noreply.github.com>
2022-09-03 14:27:12 +0800

    Add files via upload
    首次上传养殖管理系统到git

---
+++ yzgl/src/main/java/com/jerry/yzgl/YzglApplication.java
@@ -0,0 +1,13 @@
+package com.jerry.yzgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YzglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YzglApplication.class, args);
+    }
+
+}
